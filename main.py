a7238b1986387b7930b9600c0ec34126c1f87ff6
Jakub Orzyłowski <s21001@pjwstk.edu.pl>
2021-05-30 00:01:29 +0200

    Poprawki w kodzie

--- main.py
+++ main.py
@@ -5,14 +5,13 @@ from Core import Core
 
 app = QApplication(sys.argv)
 
-okno = ButtonMenu(name="Perceptron by s21001")
-rdzen = Core(okno, trainFilePath="data.csv", wordsDictPath="word_list.txt")
-
-okno.addbutton("Wypisz Dane", rdzen.show_data)
-okno.addbutton("Ocen zdanie", rdzen.ocen)
-okno.addbutton("Policz skuteczność", rdzen.skutecznosc)
-okno.addbutton("Podziel zbiór danych", rdzen.podziel_dane)
-app.exec_()
-
-
-
+okno = ButtonMenu(name="Naiwny klasyfikator Bayesa s21001")
+if len(sys.argv) >= 3:
+    rdzen = Core(okno, trainFilePath=sys.argv[1], wordsDictPath=sys.argv[2])
+    okno.addbutton("Wypisz Dane", rdzen.show_data)
+    okno.addbutton("Ocen zdanie", rdzen.ocen)
+    okno.addbutton("Policz skuteczność", rdzen.skutecznosc)
+    okno.addbutton("Podziel zbiór danych", rdzen.podziel_dane)
+    app.exec_()
+else:
+    print(f"Nie podano ścieżki do pliku z danymi treningowymi oraz word dict")
